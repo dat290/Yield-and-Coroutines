15af2b151ab3d4b89a300ebc1d685615bf72abc5
dat290 <dat290@nyu.edu>
2015-04-09 20:04:41 -0500

    Yield and Coroutines
    Unity 3D

---
+++ SampleScenes/Scripts/camSwitcher.js
@@ -0,0 +1,29 @@
+﻿/*Script for switching between cameras in your scene. First Create an empty game object
+and rename it camSwitcher or something that will signify this is the doohickey for switching cameras.*/
+
+public var firstPerson: Camera;
+public var thirdPerson: Camera;
+
+function Start() {
+thirdPerson.GetComponent.<Camera>().enabled = true;
+firstPerson.GetComponent.<Camera>().enabled = false;
+}
+
+function Update() {
+
+if (Input.GetKey(KeyCode.F1)) 
+
+{
+firstPerson.GetComponent.<Camera>().enabled = true;
+thirdPerson.GetComponent.<Camera>().enabled = false; //Pushing the "F1" key will enable a first peson perspective.
+
+}
+
+if (Input.GetKey(KeyCode.F2)) 
+
+{
+firstPerson.GetComponent.<Camera>().enabled = false;
+thirdPerson.GetComponent.<Camera>().enabled = true; //Pushing "F2" will enable a third person perspective.
+
+}
+}
